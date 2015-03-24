2cd15063ba3f9d2626881a8ecd69a8b34abb077d
Ryan King <ryanking@gmail.com>
2015-03-23 23:53:32 -0700

    Add error handling

--- lib/experian/precise_id/response.rb
+++ lib/experian/precise_id/response.rb
@@ -2,6 +2,18 @@ module Experian
   module PreciseId
     class Response < Experian::Response
 
+      def error_message
+        if @response.has_key?('Products') && @response['Products'].has_key?('PreciseIDServer') && @response['Products']['PreciseIDServer'].has_key?('Error') && @response['Products']['PreciseIDServer']['Error'].has_key?('ErrorDescription')
+          @response['Products']['PreciseIDServer']['Error']['ErrorDescription']
+        else
+          'Unknown'
+        end
+      end
+
+      def error?
+        completion_code != "0000" || (@response.has_key?('Products') && @response['Products'].has_key?('PreciseIDServer') && @response['Products']['PreciseIDServer'].has_key?('Error'))
+      end
+
       def ssn
         return '' unless @response.has_key?('Products') && @response['Products'].has_key?('PreciseIDServer') && @response['Products']['PreciseIDServer'].has_key?('Checkpoint') && @response['Products']['PreciseIDServer']['Checkpoint'].has_key?('SSNFinderDetail') && @response['Products']['PreciseIDServer']['Checkpoint']['SSNFinderDetail'].has_key?('SSNOnFile')
 
